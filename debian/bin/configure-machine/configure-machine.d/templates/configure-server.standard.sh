0ccde1899b574c78665259f778ab8460731ea061
cyclermagic <145509849+cyclermagic@users.noreply.github.com>
2024-03-03 17:54:45 +0000

    Fix file reference
    FIX: Fixed reference to standard security

--- debian/bin/configure-machine/configure-machine.d/templates/configure-server.standard.sh
+++ debian/bin/configure-machine/configure-machine.d/templates/configure-server.standard.sh
@@ -6,5 +6,5 @@ SCRIPT_FILE=$(readlink -f ${BASH_SOURCE[0]})
 SCRIPT_DIR=$(dirname ${SCRIPT_FILE})
 
 source "${SCRIPT_DIR}/_default.sh"
-source "${SCRIPT_DIR}/security.standard.sh"
+source "${SCRIPT_DIR}/security/security.standard.sh"
 echo "=== DONE ==="
