5479a169736b42d0f1ba38baa2635c0075a9ed36
cyclermagic <145509849+cyclermagic@users.noreply.github.com>
2024-03-03 19:31:43 +0000

    FIX: Download directory of the repo is now printed

--- debian/bin/download-tmp-repo.sh
+++ debian/bin/download-tmp-repo.sh
@@ -5,7 +5,7 @@ UNZIPPED_DIR_NAME="scripts-main"
 mkdir ${REPO_TMP_DIR}
 cd ${REPO_TMP_DIR}
 
-/usr/bin/wget https://github.com/cyclermagic/scripts/archive/refs/heads/main.zip
-/usr/bin/unzip main.zip
+/usr/bin/wget -q https://github.com/cyclermagic/scripts/archive/refs/heads/main.zip
+/usr/bin/unzip -q main.zip
 
-echo $REPO_TMP_DIR
+echo $REPO_TMP_DIR/${UNZIPPED_DIR_NAME}
